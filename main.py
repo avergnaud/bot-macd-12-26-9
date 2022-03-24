4fa3147230b1b939f1c75d3fd0c15ff61ec45b87
AdrienVergnaud <a.vergnaud@gmail.com>
2022-03-24 21:22:11 +0100

    pas testé lel

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import math
 import os
 
 
-def ema(data, periods):
-    multiplier = 2.0 / (periods + 1)
-    result = [ data[0] ]
+def append_macd_values(data):
+    multiplier12 = 2.0 / (12 + 1)
+    multiplier26 = 2.0 / (26 + 1)
+    data[0].update({ 'ema12': data[0]['close'] })
+    data[0].update({ 'ema26': data[0]['close'] })
     for i in range(1, len(data)):
-        result.append(result[i-1] * (1-multiplier) + data[i] * multiplier)
-    return result
-
-
-def sma(data, periods):
-    result = []
-    for i in range(periods-1, len(data)):
+        data[i].update({ 'ema12': data[i-1]['ema12'] * (1-multiplier12) + data[i]['close'] * multiplier12 })
+        data[i].update({ 'ema26': data[i-1]['ema26'] * (1-multiplier26) + data[i]['close'] * multiplier26 })
+        data[i].update({ 'macd': data[i]['ema12'] - data[i]['ema26'] })
+        if i < 9:
+            # on ne peut pas calculer le signal
+            continue
         sum = 0
-        for j in range(i-periods+1, i+1):
-            sum += data[j]
-        result.append(sum / periods)
-    return result
+        for j in range(i - 8, i + 1):
+            sum += data[j]['macd']
+        signal = sum / 9
+        data[i].update({ 'signal': signal })
 
 
 def truncate(n, decimals=0):
@@ -52,22 +53,31 @@ def hello_pubsub(event, context):
     """
     pair_symbol = "ETH/USDT"
     symbol_coin = "ETH"
-    symbol_usd = "USDT"
+    symbol_stablecoin = "USDT"
     resolution = 86400
     limit = 1000
-    myTruncate = 5
+    my_truncate = 5
 
     data = client.get_historical_prices(market=pair_symbol, resolution=resolution,
-                                        start_time=round(time.time() - resolution * limit), end_time=round(time.time()))
-    close = [x['close'] for x in data]
-
-    row = {"ema12": ema(close, 12)[-2], "ema26": ema(close, 26)[-2], "close": close[-2]}
-    print(row)
-
+                                        start_time=round(time.time() - resolution * limit),
+                                        end_time=round(time.time()))
+    append_macd_values(data)
+    row = data[-1]
     balance_coin = get_balance(symbol_coin)
-    balance_usd = get_balance(symbol_usd)
-    print(balance_coin)
-    print(balance_usd)
+    balance_stablecoin = get_balance(symbol_stablecoin)
+    print(f"{symbol_coin}={balance_coin}, {symbol_stablecoin}={balance_stablecoin}, macd={row['macd']}, signal={row['signal']}, close={row['close']}")
+    if row['macd'] > row['signal'] and balance_stablecoin > 10:
+        print("buy")
+        amount_to_buy = balance_stablecoin / row["close"]
+        # order_output = client.place_order(market=pair_symbol, side="buy", price=None, size=truncate(amount_to_buy, my_truncate), type='market')
+        # print(order_output)
+    elif row['macd'] < row['signal'] and balance_coin*row['close'] > 10:
+        print("sell")
+        amount_to_sell = balance_coin
+        # order_output = client.place_order(market=pair_symbol, side="sell", price=None, size=truncate(amount_to_sell, myTruncate), type='market')
+        # print(order_output)
+    else:
+        print("Nothing to do just wait")
 
 
 if __name__ == "__main__":
